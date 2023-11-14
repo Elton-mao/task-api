7573c50ceb75330cd4a9f44fcf9607b69af9c106
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2023-11-14 20:03:17 -0300

    configuration error handling

---
+++ taskapi/src/main/java/com/taskapi/taskapi/core/exceptions/AplicationExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.taskapi.taskapi.core.exceptions;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class AplicationExceptionHandler extends ResponseEntityExceptionHandler{
+    
+}
---
+++ taskapi/src/main/java/com/taskapi/taskapi/core/exceptions/DefaultError.java
@@ -0,0 +1,6 @@
+package com.taskapi.taskapi.core.exceptions;
+
+public class DefaultError {
+    private int error; 
+    private String message; 
+}
