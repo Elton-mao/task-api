f07b6fa8047fda6ce457aa684c3e7d97da9c9280
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2023-11-14 20:33:39 -0300

    exeptions conf3iguration

--- taskapi/src/main/java/com/taskapi/taskapi/core/exceptions/AplicationExceptionHandler.java
+++ taskapi/src/main/java/com/taskapi/taskapi/core/exceptions/AplicationExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.taskapi.taskapi.core.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
 public class AplicationExceptionHandler extends ResponseEntityExceptionHandler{
-    
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity handleExeption(Exception e){
+        log.info("teste");
+        return null; 
+    }
 }
--- taskapi/src/main/java/com/taskapi/taskapi/core/exceptions/DefaultError.java
+++ taskapi/src/main/java/com/taskapi/taskapi/core/exceptions/DefaultError.java
@@ -1,5 +1,10 @@
 package com.taskapi.taskapi.core.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class DefaultError {
     private int error; 
     private String message; 
